a355e96f5d56678a03176bc90253c73fe4d50cc6
KhaiDang <nguyendangkhai.telecom@gmail.com>
2023-07-18 19:00:32 +0700

    add screen

--- lib/Screen/payment_screen/payment_screen.dart
+++ lib/Screen/payment_screen/payment_screen.dart
@@ -1,34 +1,49 @@
+import 'package:appdatn/Screen/food_payment/food_payment_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:zoom_tap_animation/zoom_tap_animation.dart';
 
 import '../food_table/food_table_screen.dart';
+import 'payment_controller.dart';
 
 class PaymentScreen extends StatelessWidget {
-  const PaymentScreen({super.key});
+  PaymentScreen({super.key});
+
+  final controller = Get.put(PaymentController());
 
   @override
   Widget build(BuildContext context) {
     return Container(
       child: Column(
         children: [
-            Padding(
-              padding: const EdgeInsets.only(top: 20),
-              child: Center(
-                child: Image.asset(
-                  'assets/bidv.jpg',
-                  width: 400,
+          Padding(
+            padding: const EdgeInsets.only(top: 24),
+            child: Image.asset(
+              'assets/bidv.jpg',
+            ),
+          ),
+          Obx(
+                () => Visibility(
+              visible: controller.totalPayment.value > 0,
+              child: Text(
+                controller.getTotalPayment(),
+                style: TextStyle(
+                  color: Colors.red,
+                  fontSize: 14,
                 ),
               ),
             ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
           ZoomTapAnimation(
-            onTap: () {
-              showModalBottomSheet(
+            onTap: () async {
+              var tableName = await showModalBottomSheet(
                   shape: RoundedRectangleBorder(
                       borderRadius: BorderRadius.only(
                           topRight: Radius.circular(20),
-                          topLeft: Radius.circular(20)
-                      )
-                  ),
+                          topLeft: Radius.circular(20))),
                   context: context,
                   isScrollControlled: true,
                   builder: (context) {
@@ -40,6 +55,15 @@ class PaymentScreen extends StatelessWidget {
                       ),
                     );
                   });
+
+              var totalPayment = await Get.to(
+                    () => FoodPaymentScreen(),
+                arguments: {
+                  'TableName': tableName,
+                },
+              );
+
+              controller.totalPayment.value = totalPayment;
             },
             child: Container(
               height: 40,
